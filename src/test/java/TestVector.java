0d6c58949735d2aee912e549a97ecc3bdb24bebf
Yuval <nisim.yuval41@gmail.com>
2025-12-18 14:37:16 +0200

    test vectors progress

--- src/test/java/TestVector.java
+++ src/test/java/TestVector.java
@@ -2,7 +2,7 @@ import memory.SharedVector;
 import memory.VectorOrientation;
 
 public class TestVector {
-    public void testVectorCreation(){
+    public static void testVectorCreation(){
         double[] arr ={1.0,2.0,3.0};
         SharedVector testVector = new SharedVector(arr, VectorOrientation.ROW_MAJOR); 
         
@@ -10,13 +10,13 @@ public class TestVector {
         arr[0] = 999.0;
         
         // check vector init was successful 
-        TestUtils.check("Vector Creation", testVector!=null);
+        TestUtils.checkObject("Vector Creation", testVector,null);
     
         // If successful, the vector remains unchanged
-        TestUtils.check("Vector Deep Copy", testVector.get(0) == 1.0);
-        }
+        TestUtils.checkObject("Vector Deep Copy", testVector.get(0), 1.0);
+    }
     
-        public void testVectorStatistics(){
+    public static void testVectorStatistics(){
         double[] arr ={1.0,2.0,3.0};
         SharedVector testVector = new SharedVector(arr, VectorOrientation.ROW_MAJOR);
         
@@ -26,21 +26,53 @@ public class TestVector {
             str+=(testVector.get(i))+" ";
         }
         
-        TestUtils.check("Vector Get", str == "1.0 2.0 3.0 ");
-        TestUtils.check("Vector Length", testVector.length() == 3);
-        TestUtils.check("Vector Orientation", testVector.getOrientation() == VectorOrientation.ROW_MAJOR);
+        TestUtils.checkObject("Vector Get", str, "1.0 2.0 3.0 ");
+        TestUtils.checkObject("Vector Length", testVector.length(), 3);
+        TestUtils.checkObject("Vector Orientation", testVector.getOrientation(), VectorOrientation.ROW_MAJOR);
     }
-    public void testVectorAdd(){
+    
+    public static void testVectorAdd(){
         double[] arr1 ={1.0,2.0,3.0};
-        SharedVector vector1 = new SharedVector(arr1, VectorOrientation.ROW_MAJOR);
-        
         double[] arr2 ={4.0,5.0,6.0};
+        double[] arrsum1 = {5.0,7.0,9.0};
+        double[] arrsum2 ={10.0,14.0,18.0};
+        
+        SharedVector vector1 = new SharedVector(arr1, VectorOrientation.ROW_MAJOR);
         SharedVector vector2 = new SharedVector(arr2, VectorOrientation.ROW_MAJOR);
-        
-        double[] arr3 ={-1.0,-2.0,-3.0};
-        SharedVector vector3 = new SharedVector(arr3, VectorOrientation.ROW_MAJOR);
-        
+        SharedVector sumVector1 = new SharedVector(arrsum1, VectorOrientation.ROW_MAJOR);
+        SharedVector sumVector2 = new SharedVector(arrsum2, VectorOrientation.ROW_MAJOR);
 
+        vector1.add(vector2); //perform simple addition        
+        TestUtils.checkObject("Vector Simple Addition", vector1, sumVector1);
+    
+        vector1.add(vector1); //perform self addition
+        TestUtils.checkObject("Vector Self Addition", vector1, sumVector2);
+    }
+
+    public static void testNegate(){
+        double[] arr1 ={1.0,2.0,3.0};
+        double[] arr1neg ={-1.0,-2.0,-3.0};
+        double[] arr2 ={0.0,0.0};
+        double[] arr2neg ={-0.0,-0.0};
+        
+        
+        SharedVector vector1 = new SharedVector(arr1, VectorOrientation.ROW_MAJOR);
+        SharedVector vector1neg = new SharedVector(arr1neg, VectorOrientation.ROW_MAJOR);
+        SharedVector vector2 = new SharedVector(arr2, VectorOrientation.ROW_MAJOR);
+        SharedVector vector2neg = new SharedVector(arr2neg, VectorOrientation.ROW_MAJOR);
+        SharedVector nullVector=null;
+        
+        vector1.negate();
+        vector2.negate();
+        
+        TestUtils.checkObject("Negate Check", vector1, vector1neg);
+        TestUtils.checkObject("Negate Zero Check", vector2, vector2neg);
+        // try{
+        //     nullVector.negate();
+        // }
+        // catch(NullPointerException){
+            
+        // }
     }
 
 }
