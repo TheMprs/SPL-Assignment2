63138ddf4fabc6a43d2c836206f964310f7071fc
Ohad Argavan <argavano@post.bgu.ac.il>
2025-12-18 09:40:31 +0000

    SharedVector base logic

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -9,22 +9,36 @@ public class SharedVector {
     private ReadWriteLock lock = new java.util.concurrent.locks.ReentrantReadWriteLock();
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
-        // TODO: store vector data and its orientation
+        // Done: store vector data and its orientation
+        if (vector == null){throw new NullPointerException("Input vector is null");}
+        if (orientation == null){throw new NullPointerException("Input orientation is null");}
+        
+        this.orientation = orientation;
+        this.vector = new double[vector.length];
+
+        //Deep copy input vector
+        for (int i = 0; i< vector.length; i++) {
+            this.vector[i] = vector[i]; 
+        }
+    
     }
 
     public double get(int index) {
-        // TODO: return element at index (read-locked)
-        return 0;
+        // Done: return element at index (read-locked)
+        if (index < 0 || index > vector.length){
+            throw new IndexOutOfBoundsException("Iligal index");
+        }
+        return vector[index];
     }
 
     public int length() {
-        // TODO: return vector length
-        return 0;
+        // Done: return vector length
+        return vector.length;
     }
 
     public VectorOrientation getOrientation() {
-        // TODO: return vector orientation
-        return null;
+        // Done: return vector orientation
+        return orientation;
     }
 
     public void writeLock() {
@@ -44,23 +58,56 @@ public class SharedVector {
     }
 
     public void transpose() {
-        // TODO: transpose vector
+        // Done: transpose vector
+        orientation = orientation == VectorOrientation.ROW_MAJOR ? VectorOrientation.COLUMN_MAJOR : VectorOrientation.ROW_MAJOR;
     }
 
     public void add(SharedVector other) {
-        // TODO: add two vectors
+        // Done: add two vectors
+        if(other == null){throw new NullPointerException("Other vector is null");}
+
+        //Summing the result into this
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = vector[i]+ other.get(i);
+        }
     }
 
     public void negate() {
-        // TODO: negate vector
+        // Done: negate vector
+        for (Double entry : vector){
+            entry = -entry;
+        }
     }
 
+    //Assumes both vectors have the same orientation
     public double dot(SharedVector other) {
-        // TODO: compute dot product (row · column)
-        return 0;
+        // Done: compute dot product (row · column)
+        if(this.length() != other.length()){throw new IllegalArgumentException("other vector has different length from this vector");}
+        double sum = 0;
+        for (int i = 0; i < vector.length; i++) {
+            sum += vector[i]*other.get(i);
+        }
+        return sum;
     }
 
     public void vecMatMul(SharedMatrix matrix) {
         // TODO: compute row-vector × matrix
+         double[] result = new double[length()];
+         if(matrix.getOrientation() == VectorOrientation.COLUMN_MAJOR){
+             for (int i = 0; i < length(); i++) {
+                result[i] = rowxcolumnMul(matrix.get(i));
+             }
+            }
+        else{//Matrix is oriented by rows
+            //Implement
+        }
+    }
+
+    //Commputes row vector * column vector multipication
+    private double rowxcolumnMul (SharedVector other){
+        if(orientation != VectorOrientation.COLUMN_MAJOR){throw new UnsupportedOperationException("left vector is not row vector");}
+        
+        //row * column is the same as dot product <row, column transposed>
+        return dot(other);
     }
 }
