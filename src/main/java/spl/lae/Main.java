522d2d6a27aafb2779cc0e6e7a54366b43fce0ed
Yuval <nisim.yuval41@gmail.com>
2025-12-19 17:39:01 +0200

    first main

--- src/main/java/spl/lae/Main.java
+++ src/main/java/spl/lae/Main.java
@@ -1,4 +1,5 @@
 package spl.lae;
+
 import java.io.IOException;
 
 import memory.SharedVector;
@@ -6,8 +7,30 @@ import memory.VectorOrientation;
 import parser.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-      // TODO: main
+	public static void main(String[] args) throws IOException {
+        // TODO: main
+		if(args.length < 1) {
+			System.err.println("No path to input JSON file provided.");
+			return;
+		}
 
-    }
+		// Parse input arguments
+		String inputFilePath = args[0];
+		String outputFilePath = args[1];
+		int numThreads = Integer.parseInt(args[2]);
+
+		// Init Linear Algebra Engine and Input Parser
+		LinearAlgebraEngine engine = new LinearAlgebraEngine(numThreads);
+		InputParser parser = new InputParser();
+		
+		try { 
+			ComputationNode node = parser.parse(inputFilePath);
+			ComputationNode result =engine.run(node);
+			OutputWriter.write(result.getMatrix(), outputFilePath);
+		}
+		catch (Exception e) {// Catch parsing exceptions
+			OutputWriter.write(e.getMessage(), outputFilePath);
+			return;
+		}
+	}
 }
